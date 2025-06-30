8f1d87d280904b322683e66b5795f2f7d7ca2348
KevalP6 <khp90611@gmail.com>
2025-06-30 15:47:55 -0400

    Changed the prompt to handle CL input prompt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os 
 from dotenv import load_dotenv
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -8,7 +9,7 @@ from google import genai
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
-    model = 'gemini-2.0-flash-001', contents = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    model = 'gemini-2.0-flash-001', contents = sys.argv[1]
 )
 print(response.text)
 prompt_tokens = response.usage_metadata.prompt_token_count
