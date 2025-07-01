670813fd78c0bd16dfbceaa4bf836be8f0bfdb97
KevalP6 <khp90611@gmail.com>
2025-07-01 12:46:45 -0400

    Added --verbose logic to show prompt and response tokens

--- main.py
+++ main.py
@@ -6,13 +6,22 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 from google import genai
+from google.genai import types
+
 client = genai.Client(api_key=api_key)
+user_prompt = sys.argv[1]
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
 
 response = client.models.generate_content(
-    model = 'gemini-2.0-flash-001', contents = sys.argv[1]
+    model="gemini-2.0-flash-001",
+    contents=messages,
 )
-print(response.text)
-prompt_tokens = response.usage_metadata.prompt_token_count
-candidate_tokens = response.usage_metadata.candidates_token_count
 
-print (f"Prompt tokens: {prompt_tokens}\nResponse tokens: {candidate_tokens}")
+prompt_tokens = response.usage_metadata.prompt_token_count
+response_tokens = response.usage_metadata.candidates_token_count
+
+if '--verbose' in sys.argv:
+    print(f"User prompt: {user_prompt}\nPrompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}")
+else: print(response.text)
